c412faf656a094b5da13454f3b3147a1e0d54b54
Latiful_Hassan <125118940+Latiful0Hassan@users.noreply.github.com>
2026-01-27 02:10:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,37 @@
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+def mod(a, b):
+    return a % b
+
+# user = input("Please Select A for Addition, S for Subtraction, M1 for Multiplication, D for Division & M2 for Modulus:\n")
+
+# if user == "a" or user == "A":
+#     num1 = int(input("Enter your 1st num: "))
+#     num2 = int(input("Enter your 2nd num: "))
+#     print(add(num1,num2))
+# elif user == "s" or user == "S":
+#     num1 = int(input("Enter your 1st num: "))
+#     num2 = int(input("Enter your 2nd num: "))
+#     print(sub(num1,num2))
+# elif user == "m1" or user == "M1":
+#     num1 = int(input("Enter your 1st num: "))
+#     num2 = int(input("Enter your 2nd num: "))
+#     print(mul(num1,num2))
+# elif user == "d" or user == "D":
+#     num1 = int(input("Enter your 1st num: "))
+#     num2 = int(input("Enter your 2nd num: "))
+#     print(div(num1,num2))
+# elif user == "m2" or user == "M2":
+#     num1 = int(input("Enter your 1st num: "))
+#     num2 = int(input("Enter your 2nd num: "))
+#     print(mod(num1,num2))
+# else:
+#     print("Invalid!")
+    
+    
