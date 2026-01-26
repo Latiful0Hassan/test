5dc0f2b91bf40338ba206a2771ea3cc40dab89f5
Latiful_Hassan <125118940+Latiful0Hassan@users.noreply.github.com>
2026-01-27 02:18:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+# app.py
+import streamlit as st
+from calculator import add, sub, mul, div, mod
+
+st.title("Python Shikder's Calculator")
+
+# ইউজার থেকে ইনপুট নেওয়া
+num1 = st.number_input("Enter first number")
+num2 = st.number_input("Enter second number")
+
+operation = st.selectbox("Select operation", ("Add", "Sub", "Mul", "Div", "Mod"))
+
+if st.button("Calculate"):
+    if operation == "Add":
+        result = add(num1, num2)
+    elif operation == "Sub":
+        result = sub(num1, num2)
+    elif operation == "Mul":
+        result = mul(num1, num2)
+    elif operation == "Div":
+        result = div(num1, num2)
+    elif operation == "Mod":
+        result = mod(num1, num2)
+    
+    st.success(f"Result: {result}")
